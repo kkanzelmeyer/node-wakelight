0b8e5a8077b2fa0201f6088613861ea7f0319dac
Kevin Kanzelmeyer <kanzelm4@gmail.com>
2016-09-22 21:00:49 -0500

    added run method to index

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ firebase.auth().signInWithEmailAndPassword(
   alarmsRef.on('value', (data) => {
     logger.debug('ref updated!');
     lillianWakeLight.addAlarms(data.val().lillian);
+    lillianWakeLight.run();
   });
 })
 .catch((error) => {
